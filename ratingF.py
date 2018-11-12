11402da10180b45dfc28d6b31bbcaed4f2fce2e1
Aditya Prakash <adityaprakash.bobby@gmail.com>
2018-11-12 15:57:30 +0530

    forecasting code added

---
+++ ratingF.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("laptop.csv")
+
+X = df.iloc[:, 4:11].values
+Y = df.iloc[:,[11]].values
+X.shape,Y.shape
+
+from sklearn.model_selection import train_test_split
+
+Xtrain , Xtest , Ytrain , Ytest = train_test_split(X,Y)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+Xtrain = sc.fit_transform(Xtrain)
+Xtest = sc.transform(Xtest)
+
+from sklearn.linear_model import LinearRegression
+alg1 = LinearRegression()
+alg1.fit(Xtrain,Ytrain)
+
+def rating(copy):
+    Xreal = [copy]
+    Xreal = sc.transform(Xreal)
+    return alg1.predict(Xreal)
+
+copy = [15.6,1,1,2,1,2.2,498.9]
+rating(copy)
